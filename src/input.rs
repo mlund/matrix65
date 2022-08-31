ee3992972acc609fd7e20a34245b03e2e4a21c68
Mikael Lund <Mlund@mac.com>
2022-08-31 14:08:55 +0200

    Subcommands

--- src/input.rs
+++ src/input.rs
@@ -1,38 +1,47 @@
-use clap::Parser;
-
+use clap::{Parser, Subcommand};
 /// Matrix Mode Serial Communicator for MEGA65
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Push and run program file
+    #[clap(arg_required_else_help = true)]
+    Prg {
+        /// File to load (*.prg)
+        #[clap(value_parser, short = 'f')]
+        file: String,
+        /// Run after loading
+        #[clap(long, short = 'r', action)]
+        run: bool,
+    },
+
+    /// Get MEGA65 info
+    Info {},
+
+    /// Send key presses
+    #[clap(arg_required_else_help = true)]
+    Type {
+        #[clap(value_parser, short = 't')]
+        text: String,
+    },
+
+    /// List available serial devices
+    List {},
+    
+    /// Reset MEGA65
+    Reset {},
+}
+
 #[derive(Parser)]
 #[clap(version, about, long_about = None, author = "Copyright (c) 2022 Wombat - MIT Licensed")]
 pub struct Args {
-    /// Reset MEGA65
-    #[clap(long, action)]
-    pub reset: bool,
+    #[clap(subcommand)]
+    pub command: Commands,
 
     /// Serial device name
-    #[clap(short = 'p', long, required_unless_present("ports"))]
+    #[clap(short = 'p', long)]
     pub port: String,
-
-    /// Send text string (use "\r" for return)
-    #[clap(long, short = 't')]
-    pub text: Option<String>,
-
-    /// Run after loading
-    #[clap(long, short = 'r', action)]
-    pub run: bool,
-
-    /// Load program into memory
-    #[clap(long)]
-    pub prg: Option<String>,
-
-    /// List available serial devices
-    #[clap(long, action, conflicts_with("port"))]
-    pub ports: bool,
 
     /// Baud rate for serial communication
     #[clap(short = 'b', long, default_value_t = 2000000)]
     pub baud: u32,
-
-    /// Fetch MEGA65 info
-    #[clap(long, action)]
-    pub info: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,32 @@ use clap::Parser;
 mod input;
 mod serial;
 
+// /dev/cu.usbserial-AQ027F6E
+
 fn main() {
     pretty_env_logger::init();
 
     let args = input::Args::parse();
 
-    if args.ports {
-        serial::print_ports();
-    }
-
     let mut port = serial::open_port(&args.port, args.baud);
 
-    if args.info {
-        serial::hypervisor_info(&mut port);
-    }
-
-    if args.text.is_some() {
-        serial::type_text(&mut port, &args.text.unwrap().as_str());
-    }
-
-    if args.reset {
-        serial::reset(&mut port).unwrap();
-    }
-
-    if args.run {
-        serial::type_text(&mut port, "list\r");
+    match args.command {
+        input::Commands::List {} => {
+            serial::print_ports();
+        }
+        input::Commands::Info {} => {
+            serial::hypervisor_info(&mut port);
+        }
+        input::Commands::Reset {} => {
+            serial::reset(&mut port).unwrap();
+        }
+        input::Commands::Type { text } => {
+            serial::type_text(&mut port, &text.as_str());
+        }
+        input::Commands::Prg { file, run } => {
+            if run {
+                serial::type_text(&mut port, "run\r");
+            }
+        }
     }
 }
