29736c8941d0ddcf56eabb14b4d541a26d9db211
Mikael Lund <Mlund@mac.com>
2022-10-01 08:42:32 +0200

    dasm in repl interface

--- src/repl.rs
+++ src/repl.rs
@@ -1,6 +1,6 @@
 use crate::commands;
 use crate::serial;
-use reedline_repl_rs::clap::{ArgMatches, Command};
+use reedline_repl_rs::clap::{Arg, ArgMatches, Command};
 use reedline_repl_rs::{Repl, Result};
 use serialport::SerialPort;
 
@@ -19,12 +19,34 @@ pub fn start_repl(port: &mut Box<dyn SerialPort>) -> Result<()> {
         .with_banner("Welcome to matrix65!")
         .with_command(Command::new("reset").about("Reset MEGA65"), reset)
         .with_command(Command::new("go64").about("Go to C64 mode"), go64)
+        .with_command(Command::new("stop").about("Halt CPU"), stop)
+        .with_command(Command::new("start").about("Resume CPU"), start)
+        .with_command(
+            Command::new("dasm")
+                .about("Disassemble memory (prefix hex values w. 0x....)")
+                .arg(Arg::new("address").required(true))
+                .arg(Arg::new("length").required(true)),
+            peek,
+        )
         .with_command(
             Command::new("filehost").about("Start the filehost"),
             filehost,
         );
     repl.run()
 }
+
+/// Wrap peek command
+fn peek(_args: ArgMatches, context: &mut Context) -> Result<Option<String>> {
+    let address = _args.value_of("address").unwrap().to_string();
+    let length = _args.value_of("length").unwrap_or("1").to_string().parse::<usize>()?;
+    match commands::peek(context.port, address, length, None, true) {
+        Err(err) => Err(reedline_repl_rs::Error::IllegalDefaultError(
+            err.to_string(),
+        )),
+        Ok(()) => Ok(None),
+    }
+}
+
 
 /// Wrap reset command
 fn reset(_args: ArgMatches, context: &mut Context) -> Result<Option<String>> {
@@ -39,6 +61,26 @@ fn reset(_args: ArgMatches, context: &mut Context) -> Result<Option<String>> {
 /// Wrap go64 command
 fn go64(_args: ArgMatches, context: &mut Context) -> Result<Option<String>> {
     match serial::go64(context.port) {
+        Err(err) => Err(reedline_repl_rs::Error::IllegalDefaultError(
+            err.to_string(),
+        )),
+        Ok(()) => Ok(None),
+    }
+}
+
+/// Wrap stop cpu command
+fn stop(_args: ArgMatches, context: &mut Context) -> Result<Option<String>> {
+    match serial::stop_cpu(context.port) {
+        Err(err) => Err(reedline_repl_rs::Error::IllegalDefaultError(
+            err.to_string(),
+        )),
+        Ok(()) => Ok(None),
+    }
+}
+
+/// Wrap start cpu command
+fn start(_args: ArgMatches, context: &mut Context) -> Result<Option<String>> {
+    match serial::start_cpu(context.port) {
         Err(err) => Err(reedline_repl_rs::Error::IllegalDefaultError(
             err.to_string(),
         )),
